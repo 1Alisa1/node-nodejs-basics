bfb9c3e168967a5cd1d4be2a86a48da60a20ca53
Alisa <Alisa.dragun@mail.ru>
2024-01-28 22:24:28 +0100

    feat: complete tasks in streams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    createReadStream(filePath).pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const stream = fs.createWriteStream(filePath);
+
+    stdout.write('Hello\n');
+
+    stdin.on('data', (data) => {
+        stream.write(data);
+    });
 };
 
 await write();
