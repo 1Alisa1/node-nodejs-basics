923e60a91d2449c379e95358f5ff2e0aba7027aa
Alisa <Alisa.dragun@mail.ru>
2024-01-29 17:42:33 +0100

    feat: complete task in cp folder

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { spawn } from 'child_process';
+import { stdin, stdout } from 'process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node files/script.js', args, {
+    shell: true
+  });
+
+  childProcess.stdout.pipe(stdout);
+
+  stdin.pipe(childProcess.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
